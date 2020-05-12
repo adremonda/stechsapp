098ee73a02d59805b14c3b823eb6ea16eaf7a8ee
aremonda <aremonda@localhost.localdomain>
2020-05-11 21:10:07 -0300

    Se agregaron campos a la consulta

--- backend/src/utils.js
+++ backend/src/utils.js
@@ -1,12 +1,13 @@
 const makeQueryString = (modelsLengt) => {
-  let query = 'SELECT `vsi_vendor` as `vendor`, `vsi_model` as `name`, `vsi_swver` as `soft` \
+  let query = 'SELECT `vsi_vendor` as `vendor`, `vsi_model` as `name`, `vsi_swver` as `soft`, \
+`modem_macaddr` as `mac`, `ipaddr` as `ip` \
 FROM `docsis_modem`.`docsis_update` \
 WHERE `vsi_vendor` like ?';
   if(modelsLengt>0) {
     let conditions = Array(modelsLengt).fill('(`vsi_model`<> ? OR `vsi_swver`<> ?)');
     query += ' AND ' + conditions.join(' AND ');
   }
-  return query + ' GROUP BY `vendor`, `name`, `soft`';
+  return query + ' GROUP BY `vendor`, `name`, `soft`, `mac`, `ip`';
 }
 
 module.exports = {
